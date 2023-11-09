4f3f067f667d5383c15bdbf4939332814be979e2
tahanifayez <144371267+tahanifayez@users.noreply.github.com>
2023-11-09 18:17:56 +0300

    changes

--- model/User.js
+++ model/User.js
@@ -1,11 +1,13 @@
 const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
   recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
-  password:{ type: String, required: true },
+  password: { type: String, required: true },
   Bio: String,
-  Birth: Date,
-  addCategory:[{ type: Schema.Types.ObjectId, ref: "Category" }],
+  Birth: { type: Date, required: true, trim: true },
+  age: { type: Number, min: 19 },
+  addCategory: [{ type: Schema.Types.ObjectId, ref: "Category" }],
 });
 module.exports = model("User", UserSchema);
